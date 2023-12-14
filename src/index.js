8972b4c54b5ea70261adf609f3b31ba0148bd2fe
Sergey Kabanets <skabanets97@gmail.com>
2023-12-14 22:41:34 +0200

    hotfix

--- src/index.js
+++ src/index.js
@@ -29,10 +29,6 @@ let callback = (entries, observer) => {
         const { totalHits } = images;
         let results = [...images.hits];
 
-        if (page === Math.ceil(totalHits / 40)) {
-          results = [...images.hits].slice(-20);
-        }
-
         const imagesMarkup = createImagesTemplte(results);
         refs.gallery.insertAdjacentHTML('beforeend', imagesMarkup);
 
